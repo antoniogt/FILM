360f0200d6112229c1f8127866e9bc9101223b91
Antonio Guillén <antonioguillenteruel@gmail.com>
2023-11-13 11:07:09 +0100

    Initial commit

--- R/metrics_dfs.R
+++ R/metrics_dfs.R
@@ -12,28 +12,30 @@
 #' @examples
 metrics_dfs <- function(models, formula ,model_names, val,metrics){
 
+  max<-levels(val[[1]][,class])[which.max(table(val[[1]][,class]))]
+
   list_metrics <- list()
 
   class<- gsub(" ", "", unlist(strsplit(format(formula), split = "~"))[1])
 
   if(model_names %in% c("IPIP_RF","IPIP_RLOG")){
     for(i in 1:length(val)){
-    list_metrics[[i]] <- FILM::metric_probs(data.frame(
-      obs = as.factor(val[[i]][,class]),
-      pred= factor(FILM::predict_ipip(models[[i]], val[[i]][-which(colnames(val[[i]])==class)],val[[i]][,class],type="class"),labels=levels(val[[i]][,class])),
-      prob= FILM::predict_ipip(models[[i]], val[[i]][-which(colnames(val[[i]])==class)],val[[i]][,class],type="prob"),
-      obs.prob = as.numeric(ifelse(val[[i]][, class] == max, 1, 0)) # Arreglar lo de max por el nombre del level de la clase mayoritaria
-    ))[metrics]
+      list_metrics[[i]] <- FILM::metric_probs(data.frame(
+        obs = as.factor(val[[i]][,class]),
+        pred= factor(FILM::predict_ipip(models[[i]], val[[i]][-which(colnames(val[[i]])==class)],val[[i]][,class],type="class"),labels=levels(val[[i]][,class])),
+        prob= FILM::predict_ipip(models[[i]], val[[i]][-which(colnames(val[[i]])==class)],val[[i]][,class],type="prob"),
+        obs.prob = as.numeric(ifelse(val[[i]][, class] == max, 1, 0)) # Arreglar lo de max por el nombre del level de la clase mayoritaria
+      ))[metrics]
     }
   }
   else{
-  for(i in 1:length(val)){
-    list_metrics[[i]] <- FILM::metric_probs(data.frame(
-      obs = as.factor(val[[i]][,class]),
-      pred= factor(predict(models[[i]], val[[i]][-which(colnames(val[[i]])==class)]),labels=levels(val[[i]][,class])),
-      prob= predict(models[[i]], val[[i]][-which(colnames(val[[i]])==class)],type = "prob")[,max],
-      obs.prob = as.numeric(ifelse(val[[i]][, class] == max, 1, 0))
-    ))[metrics]
+    for(i in 1:length(val)){
+      list_metrics[[i]] <- FILM::metric_probs(data.frame(
+        obs = as.factor(val[[i]][,class]),
+        pred= factor(predict(models[[i]], val[[i]][-which(colnames(val[[i]])==class)]),labels=levels(val[[i]][,class])),
+        prob= predict(models[[i]], val[[i]][-which(colnames(val[[i]])==class)],type = "prob")[,max],
+        obs.prob = as.numeric(ifelse(val[[i]][, class] == max, 1, 0))
+      ))[metrics]
     }
   }
   return(list_metrics)
