0e2614d063aae941b8af903d2bb2746dda28a9a1
Antonio Guillén Teruel <73766262+antoniogt@users.noreply.github.com>
2023-11-29 21:14:29 +0100

    predict_film.R

--- R/predict_film.R
+++ R/predict_film.R
@@ -11,20 +11,20 @@
 #'
 #' @examples
 predict_film<-function(ensemble, x, y, type="class", q = 0.75){
-
+  
   min<- levels(y)[which.min(table(y))]
   max<- levels(y)[which.max(table(y))]
-
+  
   if(type=="class"){
-  pred <- data.frame(matrix(nrow=nrow(x),ncol=0))
-  for(modelo in ensemble) pred <- cbind(pred, predict(modelo,x))
-  pred <- apply(pred, 1, function(x) prop.table(table(x))[max])
-  ifelse(is.na(pred) | pred<q, min, max)
+    pred <- data.frame(matrix(nrow=nrow(x),ncol=0))
+    for(modelo in ensemble) pred <- cbind(pred, predict(modelo,x))
+    pred <- apply(pred, 1, function(x) prop.table(table(x))[max])
+    return(ifelse(is.na(pred) | pred<q, min, max))
   }
   if(type=="prob"){
     pred <- data.frame(matrix(nrow=nrow(x),ncol=0))
     for(modelo in ensemble) pred <- cbind(pred, predict(modelo,x,type="prob")[,max])
     pred<- rowSums(pred)
-    pred
+    return(pred)
   }
 }
