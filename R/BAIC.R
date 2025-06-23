ae7ee5e63224704c333bf1b69b4442359e1c802f
Antonio Guillén Teruel <73766262+antoniogt@users.noreply.github.com>
2025-06-23 11:40:53 +0200

    Update BAIC.R

--- R/BAIC.R
+++ R/BAIC.R
@@ -10,27 +10,29 @@
 #'
 #' @examples
 #'
-BAIC<-function(metrics,metric_values,props,fun=NULL){
+BAIC <- function (metrics, metric_values, props, fun = NULL) {
   value <- c()
-
-  aux <- data.frame(props=props)
-  colnames(aux)<-"p_min"
-
-  for(j in metrics){
-    c<-colnames(aux)
-    aux <- cbind(aux,data.frame(v=as.double(unlist(metric_values)[which(names(unlist(metric_values))==j)])))
-    colnames(aux)<-c(c,j)
+  aux <- data.frame(props = props)
+  colnames(aux) <- "p_min"
+  for (j in metrics) {
+    c <- colnames(aux)
+    aux <- cbind(aux, data.frame(v = as.double(unlist(metric_values)[which(sub(".*\\.", "", names(unlist(metric_values))) == 
+                                                                             j)])))
+    colnames(aux) <- c(c, j)
   }
-  cor.pe <- cor(aux,method=c("pearson"))
-  cor_pmin<- as.double(cor.pe["p_min",metrics])
-  if(is.null(fun)){
-    weights=FILM::f_gauss(cor_pmin,1,0,0.15)
-  }else{
-  weights<-fun(cor_pmin)
+  aux<- scale(aux)
+  cor.pe <- cor(aux, method = c("pearson"))
+  cor_pmin <- as.double(cor.pe["p_min", metrics])
+  if (is.null(fun)) {
+    weights = FILM::f_gauss(cor_pmin, 1, 0, 0.15)
   }
-
-  value<-as.double(c(unlist(metric_values)[metrics])) %*% weights
-
-
-  return(value)
+  else {
+    weights <- fun(cor_pmin)
+  }
+  value <- as.double(c(unlist(metric_values)[which(sub(".*\\.", "", names(unlist(metric_values))) %in% metrics & sub(".*_DF(\\d+).*", "\\1",names(unlist(metric_values))) == "1")])) %*% 
+    weights
+  return(list(
+    BAIC_value = value,
+    cor_pmin  = cor_pmin
+  ))
 }
