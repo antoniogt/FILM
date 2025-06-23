db02103948911e73a08ac662018ef8a8641a40af
Antonio Guillén Teruel <73766262+antoniogt@users.noreply.github.com>
2025-06-23 11:30:51 +0200

    Update BAIC.R

--- R/UIC.R
+++ R/UIC.R
@@ -1,8 +1,8 @@
-#' UIC metric function
+#' BAIC metric function
 #'
 #' @param metrics: Metric names
-#' @param metric_values: Metric values in a list to calculate the UIC value
-#' @param props: Proportion of minority class of each dataset to get the UIC value
+#' @param metric_values: Metric values in a list to calculate the BAIC value
+#' @param props: Proportion of minority class of each dataset to get the BAIC value
 #' @param fun: Function to obtain the weights of each metric
 #'
 #' @return number
@@ -10,7 +10,7 @@
 #'
 #' @examples
 #'
-UIC<-function(metrics,metric_values,props,fun=NULL){
+BAIC<-function(metrics,metric_values,props,fun=NULL){
   value <- c()
 
   aux <- data.frame(props=props)
